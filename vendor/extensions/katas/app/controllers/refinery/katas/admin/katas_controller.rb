da655c87524f18e66c30947657853b40b490678e
Libby Schumacher-Knight <libby@fluxfederation.com>
2018-01-26 15:55:33 +1300

    WIP Add checkboxes to admin form for edit/update

--- vendor/extensions/katas/app/controllers/refinery/katas/admin/katas_controller.rb
+++ vendor/extensions/katas/app/controllers/refinery/katas/admin/katas_controller.rb
@@ -3,13 +3,40 @@ module Refinery
     module Admin
       class KatasController < ::Refinery::AdminController
 
+        before_action :find_all_stances
+
         crudify :'refinery/katas/kata'
+
+        def update
+          @kata = Kata.find(params[:id])
+
+          if @kata.update(kata_params)
+            stance_params.each do |stance_id|
+              KatasStance.create(kata_id: @kata.id, stance_id: stance_id)
+            end
+
+            flash[:notice] = "#{@kata.title} was successfully updated."
+            redirect_to katas_admin_katas_path
+          else
+            render :edit
+          end
+        end
+
+        protected
+
+        def find_all_stances
+          @stances = Refinery::Stances::Stance.all
+        end
 
         private
 
         # Only allow a trusted parameter "white list" through.
         def kata_params
           params.require(:kata).permit(:title, :meaning, :interpretation)
+        end
+
+        def stance_params
+          params.require(:stance_ids)
         end
       end
     end
